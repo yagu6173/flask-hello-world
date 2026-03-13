bd0f68feb9e9d7e3802aec47757650bb1f208d17
Yan Gu <nju.guyan@gmail.com>
2026-03-12 21:24:44 -0700

    Implement CRUD operations for Basketball table in app.py

--- app.py
+++ app.py
@@ -33,3 +33,69 @@ def testing():
     conn.close()
     return "Database Connection Test Successful"
 
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect(db_url)
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Creation Successful"
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect(db_url)
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect(db_url)
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fetchall()
+    conn.close()
+
+    # convert query result to html table by a for loop
+    table = "<table>"
+    for player in records:
+        table += "<tr>"
+        for info in player:
+            table += "<td>{}</td>".format(info)
+        table += "</tr>"
+    table += "</table>"     
+    return table
+
+@app.route('/db_drop')
+def dropping():
+    conn = psycopg2.connect(db_url)
+    cur = conn.cursor()
+    cur.execute('''
+        DROP TABLE Basketball;
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Dropped"
+
+
+
